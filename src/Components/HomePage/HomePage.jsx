2f7d6118f32fd18dc27044d6b4d07005a4adb317
trdmitr <trdmitr@gmail.com>
2024-03-03 00:19:32 +0400

    color etc

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 export const HomePage = () => {
-    const imgUrl = "https://trv-axtuba.ru/wp-content/uploads/2024/02/23-fevralya.jpg"
+    const imgUrl = "https://sun9-38.userapi.com/impg/aG4rTmMa0sKD5SefYkinhGpOAbYqyGIi9js1fg/fCY6QR2_AAU.jpg?size=768x1152&quality=96&sign=570e29395de39a9bb08f60395d72796d&type=album"
     // const imgUrl2 = 'https://trv-axtuba.ru/wp-content/uploads/2024/02/23-fevralya.jpg'
     const imgAlt = "Каверы Подземки"
     
@@ -16,7 +16,7 @@ export const HomePage = () => {
               <div className="device-content">
 
                   <div>
-                      <span className={classes.navLink}>Поздравления девушек!</span>
+                      <span className={classes.navLink}>Парни поздравляют с 8 марта 2024!</span>
                       {/* <Link to="/cavers21"  className={classes.navLink21}>2021</Link>
                       <Link to="/cavers22"  className={classes.navLink22}>2022</Link> */}
                       <Link to="/cavers23"  className={classes.navLink23}>Слушаем!</Link>
